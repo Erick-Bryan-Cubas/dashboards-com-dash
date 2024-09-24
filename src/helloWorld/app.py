1f3cbf21f9aa8985191230c71ad1bfb4ea490f97
Erick-Bryan-Cubas <thecubas@outlook.com>
2024-09-24 13:38:41 -0300

    :construction: Layout customization

--- src/helloWorld/app.py
+++ src/helloWorld/app.py
@@ -4,8 +4,9 @@ from dash import html, dcc
 import plotly.express as px
 import pandas as pd 
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 df = pd.DataFrame({
@@ -22,8 +23,8 @@ fig = px.bar(df, x="Fruit", y="Amount", color="City")
 ### Dash HTML Components
 app.layout = html.Div(id="div1",
     children=[
-        html.H1("Hello Dash", id="h1"),        
-        html.Div("Dash: A web application framework for Python."),
+        html.H1("Hello Dash", id="h1", style={"textAlign": "center", "color": "#023e8a"}),
+        html.Div("Dash: A web application framework for Python.", id="div2", style={"textAlign": "center", "color": "#023e8a"}),
         #### Dash Core Components
         dcc.Graph(figure=fig, id="graph")
     ]
